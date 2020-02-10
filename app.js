df27a7d58e73f89c1d17997ee87e72c6db503321
Nat <nat.wrw@gmail.com>
2020-02-10 18:43:30 +0700

    add port.write msg

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let port = new SerialPort("COM1", { baudRate: 9600 });
 port.on("open", () => {
 	console.log("port opened.");
 	const msg = Buffer.from(
-		[40, 0, 1, 254, 217]);
+		[40, 0, 1, 245, 226]);
 	port.write(msg);
 });
 
@@ -20,5 +20,5 @@ port.on("error", e => {
 });
 
 port.on("data", data => {
-	console.log(`Recv: `, data);
+	console.log(`Recv: `, data.toString());
 });
