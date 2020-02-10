bd2ce6928ed6d45194e14151f4e44dc09bc090df
Nat <nat.wrw@gmail.com>
2020-02-10 20:03:11 +0700

    update command.

--- write.js
+++ write.js
@@ -1,11 +1,50 @@
 const mqtt = require("mqtt");
 let client = mqtt.connect("mqtt://mqtt.cmmc.io");
 let { checksum } = require("./checksum");
+let CMD = require("./CMD");
 
-client.on("connect", function() {
-	let d = [40, 0, 1, 245];
-	let chksum = checksum(d);
-	let b = Uint8Array.from([...d, chksum]);
-	client.publish("BILL_VALIDATOR", b.join(","));
+function timeout(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+client.on("connect", async () => {
+	let d;
+	let b;
+
+	d = [40, 0, 1, CMD.reset_device];
+	b = Uint8Array.from([...d, checksum(d)]);
+	await client.publish("BILL_VALIDATOR", b.join(","));
+	await timeout(2000);
+
+	d = [40, 0, 1, CMD.request_software_revision];
+	b = Uint8Array.from([...d, checksum(d)]);
+	await client.publish("BILL_VALIDATOR", b.join(","));
+	await timeout(500);
+
+	d = [40, 0, 1, CMD.simple_poll];
+	b = Uint8Array.from([...d, checksum(d)]);
+	await client.publish("BILL_VALIDATOR", b.join(","));
+	await timeout(500);
+
+	d = [40, 0, 1, CMD.perform_self_check];
+	b = Uint8Array.from([...d, checksum(d)]);
+	await client.publish("BILL_VALIDATOR", b.join(","));
+	await timeout(500);
+
+	d = [40, 2, 1, CMD.modify_inhibit_status, 255, 255];
+	b = Uint8Array.from([...d, checksum(d)]);
+	await client.publish("BILL_VALIDATOR", b.join(","));
+	await timeout(500);
+
+	//
+	d = [40, 0, 1, CMD.modify_master_inhibit_status];
+	b = Uint8Array.from([...d, checksum(d)]);
+	await client.publish("BILL_VALIDATOR", b.join(","));
+	await timeout(500);
+
+	d = [40, 0, 1, CMD.read_buffered_bill_events];
+	b = Uint8Array.from([...d, checksum(d)]);
+	await client.publish("BILL_VALIDATOR", b.join(","));
+	await timeout(500);
 	client.end();
 });
