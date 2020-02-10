b8385fb27f196d89cbd38949baefa7ac6072e49d
Nat <nat.wrw@gmail.com>
2020-02-10 18:45:05 +0700

    add port.write msg

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ let port = new SerialPort("COM1", { baudRate: 9600 });
 port.on("open", () => {
 	console.log("port opened.");
 	const msg = Buffer.from(
-		[40, 0, 1, 245, 226]);
+		//[40, 0, 1, 245, 226]);
+		[40, 0, 1, 254, 217]);
 	port.write(msg);
 });
 
